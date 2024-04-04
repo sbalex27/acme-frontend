c90c925e8bd730d2b48b242b370a103ff1b34048
Alex <alex2000.sabe@gmail.com>
2024-04-04 10:44:08 -0600

    Validation errors

--- components/dynamic-form.vue
+++ components/dynamic-form.vue
@@ -6,12 +6,41 @@
       <h1 class="text-1xl font-semibold">{{ form.description }}</h1>
     </div>
 
+    <!-- Success -->
+    <div
+      v-if="success"
+      class="bg-green-200 text-green-700 text-sm p-4 rounded-lg mb-4"
+    >
+      <div class="text-green-700 text-lg mb-1 font-semibold">
+        Formulario Enviado
+      </div>
+      <div>Gracias por enviar el formulario.</div>
+    </div>
+
     <!-- Guest Name -->
     <div class="mb-4">
       <UInput
         v-model="guestName"
         placeholder="Enviar formulario a nombre de..."
       ></UInput>
+    </div>
+
+    <!-- Errors -->
+    <div
+      v-if="errors !== null"
+      class="bg-red-200 text-red-700 text-sm p-4 rounded-lg mb-4"
+    >
+      <div class="text-red-700 text-lg mb-1 font-semibold">
+        Formulario Inválido
+      </div>
+
+      <ul>
+        <li v-for="errorField in errors" :key="errorField">
+          <div v-for="error of errorField" :key="error">
+            <div>{{ error }}</div>
+          </div>
+        </li>
+      </ul>
     </div>
 
     <!-- Fields -->
@@ -21,8 +50,7 @@
 
     <!-- Submit Button -->
     <div class="mt-6 flex justify-end">
-      <UButton
-        @click="submitForm"
+      <UButton :loading="loading" @click="submitForm" :disabled="success"
         >Enviar</UButton
       >
     </div>
@@ -39,6 +67,9 @@ export default {
   data() {
     return {
       guestName: "",
+      errors: null,
+      success: false,
+      loading: false,
     };
   },
   props: {
@@ -63,20 +94,31 @@ export default {
   },
   methods: {
     async submitForm() {
+      this.errors = null;
+
       const request = {
         filledBy: this.guestName,
         fields: this.resultFields,
       };
 
-      const response = await $fetch(this.sendFormUrl, {
+      this.loading = true;
+
+      await $fetch(this.sendFormUrl, {
         method: "POST",
         body: JSON.stringify(request),
         headers: {
           "Content-Type": "application/json",
         },
-      });
-
-      console.log(response);
+      })
+        .then((response) => {
+          this.success = true;
+        })
+        .catch((err) => {
+          this.errors = err.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
   },
 };
